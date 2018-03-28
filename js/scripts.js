d3d94f5462a7ed0ffb816a85c3247e10f707aada
Hubert <hubert.art@gmail.com>
2018-03-28 11:18:08 +0200

    Traingle Area Script

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var a = prompt("Enter value A:");
+var h = prompt("Enter value H:");
+
+var triangleArea = [a * h / 2];
+
+alert("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
+
+//console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
+
+
+
+
+
+/*
+var a = 2,
+    h = 6;
+
+var triangleArea = [a * h / 2];
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+*/

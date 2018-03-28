638906b41ff1b04a5e176a798799b06e69da61ce
Hubert <hubert.art@gmail.com>
2018-03-28 14:28:59 +0200

    JS script correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var a = prompt("Enter value A:");
 var h = prompt("Enter value H:");
 
-var triangleArea = [a * h / 2];
+var triangleArea = a * h / 2;
 
 alert("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
 
